8b8c5cfb4b44e964ca67e2d6121590018ad5826b
Maria Mai <mariamaihong1309@gmail.com>
2023-03-12 19:24:40 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar"
+
+const Navbar = () => (
+    <Stack
+        direction='row'
+        alignItems="center"
+        p={2}
+        sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
+        <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+            <img src={logo} alt="logo" height={45} />
+            <span style={{ color: "white", marginLeft: "15px", fontWeight: "bolder", fontSize: "22px" }}>
+                Phuc Mai Media
+            </span>
+        </Link>
+        <SearchBar />
+    </Stack>
+)
+
+export default Navbar
---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon />, },
+    { name: 'Coding', icon: <CodeIcon />, },
+    { name: 'ReactJS', icon: <CodeIcon />, },
+    { name: 'NextJS', icon: <CodeIcon />, },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Fashion', icon: <CheckroomIcon />, },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+    { name: 'Gym', icon: <FitnessCenterIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/5e4zONtMe0w/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhyIEYoMjAP&rs=AOn4CLC6HEycujpFrGr1Sx_qz-ffEHBqpg';
+export const demoChannelUrl = '/MobGamingYT';
+export const demoVideoUrl = '/video/5e4zONtMe0w';
+export const demoChannelTitle = 'Mob Gaming YT';
+export const demoVideoTitle = 'Valorant The Boys Meme';
+export const demoProfilePicture = 'https://yt3.ggpht.com/ytc/AL5GRJXGHRu-6g7TlFm7p6OLXaIoQJYppB3JL8kGTcoNjg=s88-c-k-c0x00ffffff-no-rj'
